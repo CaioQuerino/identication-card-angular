244b8b6580d90d756653806e61b5595c962c363f
Caio Querino <caioquerino04@gmail.com>
2025-09-04 00:07:57 -0300

    feat: adicionando card na HomePage

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonCard, 
+  IonCardHeader, 
+  IonCardTitle, 
+  IonCardSubtitle, 
+  IonCardContent, 
+  IonAvatar, 
+  IonIcon, 
+  IonList, 
+  IonItem, 
+  IonLabel, 
+  IonButton, 
+  IonGrid, 
+  IonRow, 
+  IonCol 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  person, 
+  school, 
+  mail, 
+  call, 
+  informationCircle, 
+  logoGithub, 
+  logoLinkedin, 
+  logoInstagram 
+} from 'ionicons/icons';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonAvatar,
+    IonIcon,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonButton,
+    IonGrid,
+    IonRow,
+    IonCol
+  ]
+})
+export class HomePage {
+
+  constructor(private router: Router) {
+    addIcons({ 
+      person, school, mail, call, informationCircle,
+      logoGithub, logoLinkedin, logoInstagram 
+    });
+  }
+
+  navigateToAbout() {
+    this.router.navigate(['/about']);
+  }
+}
