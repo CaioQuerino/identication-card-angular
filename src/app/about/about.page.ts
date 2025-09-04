4723f40a4c0680f9fee13e08dacbafeae438f26f
Caio Querino <caioquerino04@gmail.com>
2025-09-04 17:41:48 -0300

    refactor: removendo importação não utilizada do Router

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { 
   IonHeader, 
   IonToolbar, 
