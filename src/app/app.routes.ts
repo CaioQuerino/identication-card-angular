1097548324b50bd9c58bf3e203c7ce587d1bea0f
Caio Querino <caioquerino04@gmail.com>
2025-09-04 00:18:30 -0300

    refactor: configurações default do Angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.page').then(m => m.AboutPage)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
